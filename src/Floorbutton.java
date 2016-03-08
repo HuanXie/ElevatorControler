8dd839497613fd9aef41fb539e0d3c09c93f7b08
chenzimin <chen.zimin@hotmail.com>
2016-03-08 13:30:25 +0100

    Comment init and floorbutton

--- src/Floorbutton.java
+++ src/Floorbutton.java
@@ -1,3 +1,7 @@
+/*
+ * This class represent up and down button at elevator for each floor
+ */
+
 public class Floorbutton {
 	public boolean up; // If up button at this floor is pressed
 	public boolean down; // If down button at this floor is pressed
--- src/Init.java
+++ src/Init.java
@@ -1,22 +1,27 @@
+/*
+ * This class initialize all elevators and main controller and starts them
+ */
 import java.util.Scanner;
 
 public class Init {
 	public static void main(String[] args)
 	{
-		Scanner s = new Scanner(System.in);
-		System.out.println("Number of elevators? ");
-		int numberOfElevators = s.nextInt();
-		System.out.println("Number of floors? ");
-		int numberOfFloors = s.nextInt();
-		s.close();
+		Scanner s = new Scanner(System.in); // Scanner to read input
+		System.out.println("Number of elevators? "); // Ask user
+		int numberOfElevators = s.nextInt(); // Read input
+		System.out.println("Number of floors? "); // Ask user
+		int numberOfFloors = s.nextInt(); // Read input
+		s.close(); // Close scanner
 		
-		Elevator[] elevators = new Elevator[numberOfElevators];
-		MainController controller = new MainController(numberOfElevators, numberOfFloors, elevators);
+		Elevator[] elevators = new Elevator[numberOfElevators]; // Create elevators
+		MainController controller = new MainController(numberOfElevators, numberOfFloors, elevators); // Create controller
+		// Initiallize all elevators
 		for(int i = 0; i < elevators.length; i++)
 		{
 			elevators[i] = new Elevator(i+1,numberOfFloors,controller);
 		}
-		controller.start();
+		controller.start(); // Start controller
+		// Start all elevators
 		for(int i = 0; i < elevators.length; i++)
 		{
 			elevators[i].start();
